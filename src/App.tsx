28cff11afdd6601f30eb7e95ad8ddeb99fdecbb2
GedeonRO <otakuagbolossou@gmail.com>
2024-10-21 21:47:51 +0000

    feat : add sidebar

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="bg-[#fff] w-full h-full flex ">
+      <Sidebar />
+      <main className="bg-[#E5E5E5] w-full">
+        <h1>text</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,43 @@
+import {
+  CalendarAdd,
+  Category,
+  Chart2,
+  DollarSquare,
+  Setting2,
+} from "iconsax-react";
+
+export default function Sidebar() {
+  return (
+    <div className="bg-[#fff] items-center w-[250px] ">
+      <div className="flex flex-col justify-between h-screen">
+        <div className="flex flex-col gap-[48px]">
+          <span className="pl-6 py-[24px] text-4xl font-bold text-[#605BFF]">
+            {"<Dev />"}
+          </span>
+          <div className="flex flex-col gap-[12px]">
+            <div className="py-[12px] pl-[24px] flex gap-[12px] hover:text-[#605BFF] items-center hover:bg-gradient-to-r hover:from-[#ACA9FF] hover:via-[#fff] hover:to-[#fff] ">
+              <Category variant="Bold" />
+              <span className="font-semibold text-inherit">Dashboard</span>
+            </div>
+            <div className="py-[12px] pl-[24px] flex gap-[12px] hover:text-[#605BFF] items-center hover:bg-gradient-to-r hover:from-[#ACA9FF] hover:via-[#fff] hover:to-[#fff]">
+              <DollarSquare variant="Bold" />
+              <span className="font-semibold text-inherit">Invoice</span>
+            </div>
+            <div className="py-[12px] pl-[24px] flex gap-[12px] hover:text-[#605BFF] items-center hover:bg-gradient-to-r hover:from-[#ACA9FF] hover:via-[#fff] hover:to-[#fff]">
+              <Chart2 variant="Bold" />
+              <span className="font-semibold text-inherit">Analytics</span>
+            </div>
+            <div className="py-[12px] pl-[24px] flex gap-[12px] hover:text-[#605BFF] items-center hover:bg-gradient-to-r hover:from-[#ACA9FF] hover:via-[#fff] hover:to-[#fff]">
+              <CalendarAdd variant="Bold" />
+              <span className="font-semibold text-inherit">Schedule</span>
+            </div>
+            <div className="py-[12px] pl-[24px] flex gap-[12px] hover:text-[#605BFF] items-center hover:bg-gradient-to-r hover:from-[#ACA9FF] hover:via-[#fff] hover:to-[#fff]">
+              <Setting2 variant="Bold" />
+              <span className="font-semibold text-inherit">Settings</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
