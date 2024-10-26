1153d2b51a1b44c7492dc1d14991625b8a20bf79
GedeonRO <otakuagbolossou@gmail.com>
2024-10-26 10:44:41 +0000

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
     <div className="bg-[#fff] w-full h-full flex ">
       <Sidebar />
       <main className="bg-[#E5E5E5] w-full p-[20px]">
-        <h1>text</h1>
+        <Dashboard />
       </main>
     </div>
   );
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,60 @@
+import { Bag2 } from "iconsax-react";
+import { ChartDot } from "@/components/ChartLine";
 export default function Dashboard() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col gap-[32px] w-full">
+      <div className="flex w-full justify-between items-center">
+        <span className="text-2xl font-bold">Dashboad</span>
+        <button>Add New + </button>
+      </div>
+      <div className="grid col-span-4 grid-cols-4 gap-[24px] w-full">
+        <div className="bg-[#fff] rounded-[10px] w-full py-[12px] px-[24px] max-h-max flex gap-[24px] items-center justify-start">
+          <div className="bg-[#fff4f0] rounded-full size-[72px] flex items-center justify-center">
+            <Bag2 size={40} color="#FF8F6B" />
+          </div>
+          <div className="flex flex-col ">
+            <span className="font-bold text-[#030229] text-2xl">190+</span>
+            <span className="text-[#030229] text-sm">sales products</span>
+          </div>
+        </div>
+        <div className="bg-[#fff] rounded-[10px] w-full py-[12px] px-[24px] max-h-max flex gap-[24px] items-center justify-start">
+          <div className="bg-[#fff4f0] rounded-full size-[72px] flex items-center justify-center">
+            <Bag2 size={40} color="#FF8F6B" />
+          </div>
+          <div className="flex flex-col ">
+            <span className="font-bold text-[#030229] text-2xl">190+</span>
+            <span className="text-[#030229] text-sm">sales products</span>
+          </div>
+        </div>
+        <div className="bg-[#fff] rounded-[10px] w-full py-[12px] px-[24px] max-h-max flex gap-[24px] items-center justify-start">
+          <div className="bg-[#fff4f0] rounded-full size-[72px] flex items-center justify-center">
+            <Bag2 size={40} color="#FF8F6B" />
+          </div>
+          <div className="flex flex-col ">
+            <span className="font-bold text-[#030229] text-2xl">190+</span>
+            <span className="text-[#030229] text-sm">sales products</span>
+          </div>
+        </div>
+        <div className="bg-[#fff] rounded-[10px] w-full py-[12px] px-[24px] max-h-max flex gap-[24px] items-center justify-start">
+          <div className="bg-[#fff4f0] rounded-full size-[72px] flex items-center justify-center">
+            <Bag2 size={40} color="#FF8F6B" />
+          </div>
+          <div className="flex flex-col ">
+            <span className="font-bold text-[#030229] text-2xl">190+</span>
+            <span className="text-[#030229] text-sm">sales products</span>
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-5 col-span-5 w-full gap-[24px]">
+        <div className="col-span-3">
+          <ChartDot />
+        </div>
+        <div className="col-span-2"></div>
+      </div>
+      <div className="grid grid-cols-5 col-span-5 w-full gap-[24px]">
+        <div className="col-span-3"></div>
+        <div className="col-span-2"></div>
+      </div>
+    </div>
+  );
 }
